6b6045c4d0b245223101b7217eab31183116136f
Darshan KT <54614620+darshan-kt@users.noreply.github.com>
2022-12-14 15:51:07 +0530

    Update pub_sub_v1_launch.py

--- ros2_pkg/launch/pub_sub_v1_launch.py
+++ ros2_pkg/launch/pub_sub_v1_launch.py
@@ -2,8 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    pub_cmd = Node(package="my_pkg", node_executable="publisher_node", output="screen")
-    sub_cmd = Node(package="my_pkg", node_executable="subscriber_node", output="screen")
+    pub_cmd = Node(package="ros2_pkg", node_executable="publisher_node", output="screen")
+    sub_cmd = Node(package="ros2_pkg", node_executable="subscriber_node", output="screen")
 
     ld = LaunchDescription()
     ld.add_action(pub_cmd)
