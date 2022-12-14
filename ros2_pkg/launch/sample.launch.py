06c83fa95dc0a30673392a7810260cba1ce83944
Darshan KT <54614620+darshan-kt@users.noreply.github.com>
2022-12-14 15:52:53 +0530

    Update sample.launch.py

--- ros2_pkg/launch/sample.launch.py
+++ ros2_pkg/launch/sample.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
   print("urdf_file_name : {}".format(urdf_file_name))
 
   urdf = os.path.join(
-      get_package_share_directory('my_pkg'),
+      get_package_share_directory('ros2_pkg'),
       urdf_file_name)
 
   return LaunchDescription([
